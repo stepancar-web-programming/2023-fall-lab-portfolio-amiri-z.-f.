24077f2b48e5b444eda70999807fe0570e6fec6f
Zirullo Amiri <amirymax@gmail.com>
2024-01-22 17:27:13 +0300

    Сделаны требуемые изменения

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-$(document).ready(function () {
-    $('#video-section').html('<p><b>The best goal ever! It was better than everything!</b></p>  <iframe width="560" height="315" src="https://www.youtube.com/embed/ZC_XihZGPEg?si=KAz7HItciZuP2wU3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
- // Toggle menu
-    $('#toggle-menu').on('click', function () {
-        $('nav').toggleClass('show-menu');
+document.addEventListener("DOMContentLoaded", function () {
+  var videoSection = document.getElementById("video-section");
+  if(videoSection!=null){
+  videoSection.innerHTML =
+    '<p><b>The best goal ever! It was better than everything!</b></p> <iframe width="560" height="315" src="https://www.youtube.com/embed/ZC_XihZGPEg?si=KAz7HItciZuP2wU3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+}
+  document.getElementById("toggle-menu").addEventListener("click", function () {
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("show-menu");
+  });
+
+  document
+    .getElementById("toggle-dark-mode")
+    .addEventListener("click", function () {
+      var body = document.body;
+      body.classList.toggle("dark-mode");
+    });
+   
+  document
+    .getElementById("toggle-dark-mode-nav")
+    .addEventListener("click", function () {
+      var body = document.body;
+      body.classList.toggle("dark-mode");
     });
 
-    // Toggle dark mode
-    $('#toggle-dark-mode').on('click', function () {
-        $('body').toggleClass('dark-mode');
-    });
-
-    // Close menu when clicking on the page
-    $(document).on('click', function (event) {
-        if (!$(event.target).closest('nav').length && !$(event.target).is('#toggle-menu')) {
-            $('nav').removeClass('show-menu');
-        }
-    });
-    
+  document.addEventListener("click", function (event) {
+    var nav = document.querySelector("nav");
+    if (!event.target.closest("nav") && event.target.id !== "toggle-menu") {
+      nav.classList.remove("show-menu");
+    }
+  });
 });
